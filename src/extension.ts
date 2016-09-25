69ef79b6900dfe3606d4956622e01180ccd9bfe5
Pine Wu <t-zhwu@microsoft.com>
2016-09-25 15:47:30 -0700

    Init

---
+++ src/extension.ts
@@ -0,0 +1,111 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import { DefinitionProvider, Uri, TextDocument, Position, CancellationToken, Range, Location, Definition } from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as postcss from 'postcss';
+import * as _  from 'lodash';
+
+export function activate(context: vscode.ExtensionContext) {
+  const rootPath = vscode.workspace.rootPath;
+  const tachyonsPath = path.join(rootPath, 'node_modules/tachyons/css/tachyons.css');
+
+  fs.stat(tachyonsPath, (err) => {
+    if (err) {
+      return;
+    }
+
+    const provider = new TachyonsDefinitionProvider(tachyonsPath);
+
+    vscode.languages.registerDefinitionProvider('html', provider);
+  });
+}
+
+export function deactivate() {
+
+}
+
+class TachyonsDefinitionProvider implements DefinitionProvider {
+  private _tachyonsUri: Uri;
+  private _definitionMap: Map<string, Definition>;
+
+  constructor(tachyonsPath: string) {
+    this._tachyonsUri = Uri.file(tachyonsPath);
+    this.generateDefinitionMap();
+  }
+
+  private generateDefinitionMap(): void {
+    this._definitionMap = new Map();
+
+    const tachyonsSource = fs.readFileSync(this._tachyonsUri.fsPath, 'utf-8');
+    postcss().process(tachyonsSource).then(result => {
+      result.root.walkRules(rule => {
+        const { selectors, source } = rule;
+
+        selectors.forEach(selector => {
+          if (this.isClassSelector(selector)) {
+            this._definitionMap.set(selector, this.getDefinition(source));
+          }
+        });
+      });
+    })
+  }
+
+  private getDefinition(source: postcss.NodeSource): Definition {
+    const start = new Position(source.start.line - 1, source.start.column);
+    const end   = new Position(source.end.line   - 1, source.end.column);
+
+    return new Location(this._tachyonsUri, new Range(start, end));
+  }
+
+  provideDefinition(document: TextDocument, position: Position, token: CancellationToken): Definition {
+    const currWordRange = document.getWordRangeAtPosition(position);
+
+    // Ensure the current word is valid
+    if (!currWordRange) {
+      return;
+    }
+
+    // Ensure the current word is html attributes enclosed by quote
+    const currWord = document.getText(currWordRange);
+    if (!_.startsWith(currWord, `"`) && !_.startsWith(currWord, `'`)) {
+      return;
+    }
+
+    // If cursor is before beginning quote or after trailing quote, provide nothing
+    if (position.character === currWordRange.start.character || position.character === currWordRange.end.character) {
+      return;
+    }
+
+    // Ensure the current word is a class attr
+    if (!this.isClassAttr(document, currWordRange.start)) {
+      return;
+    }
+
+    const classAtPosition = '.' + this.getClassAtPosition(document, position, currWordRange, currWord);
+    if (this._definitionMap.has(classAtPosition)) {
+      return this._definitionMap.get(classAtPosition);
+    }
+  }
+
+  private isClassSelector(selector: string) {
+    return selector[0] === '.';
+  }
+
+  private getClassAtPosition(document: TextDocument, position: Position, currWordRange: Range, currWord: string): string {
+    const classes = _.trim(currWord, `"'`).split(' ');
+    const positionOffset = position.character - currWordRange.start.character;
+    let startOffset = 0;
+    return classes.find(c => {
+      startOffset += 1 + c.length;
+      return positionOffset <= startOffset;
+    });
+  }
+
+  private isClassAttr(document: TextDocument, start: Position) {
+    var classAttrPosition = new Position(start.line, start.character - 2);
+    const classWordRange = document.getWordRangeAtPosition(classAttrPosition);
+    return classWordRange && document.getText(classWordRange) === 'class';
+  }
+}
